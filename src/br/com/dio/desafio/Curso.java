9af1d244b8414f3b96a37ab20ad19b055723d9e5
Maxwell Dantas <mdszdev@gmail.com>
2022-04-04 14:23:04 -0300

    Refatorando Curso e Mentoria que estendem de Conteudo

--- src/br/com/dio/desafio/Curso.java
+++ src/br/com/dio/desafio/Curso.java
@@ -1,34 +1,17 @@
 package br.com.dio.desafio;
 
-public class Curso {
+import br.com.dio.desafio.dominio.Conteudo;
 
-    private String titulo;
-    private String descricao;
+public class Curso extends Conteudo {
+
     private int cargaHoraria;
 
     public Curso() {
     }
 
-    public Curso(String titulo, String descricao, int cargaHoraria) {
-        this.titulo = titulo;
-        this.descricao = descricao;
-        this.cargaHoraria = cargaHoraria;
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO * cargaHoraria;
     }
 
     public int getCargaHoraria() {
@@ -42,8 +25,8 @@ public class Curso {
     @Override
     public String toString() {
         return "Curso{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
                 ", cargaHoraria=" + cargaHoraria +
                 '}';
     }
