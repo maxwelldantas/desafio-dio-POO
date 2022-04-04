8deb4d8ed250ba1acaf9dfe88fc122decb899711
Maxwell Dantas <mdszdev@gmail.com>
2022-04-03 23:31:50 -0300

    Mentoria e Curso

---
+++ src/br/com/dio/desafio/Main.java
@@ -0,0 +1,28 @@
+package br.com.dio.desafio;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso1.setTitulo("Curso JS");
+        curso1.setDescricao("Descrição curso JS");
+        curso1.setCargaHoraria(8);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria Java");
+        mentoria.setDescricao("Descrição mentoria Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+    }
+}
